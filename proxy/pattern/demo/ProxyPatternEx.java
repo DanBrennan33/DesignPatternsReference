2d31da9dd066b061d923ea97322c919f55e874be
Dan Brennan <DanBrennan33@hotmail.com>
2018-10-18 17:14:13 -0400

    Add files via upload

---
+++ proxy/pattern/demo/ProxyPatternEx.java
@@ -0,0 +1,9 @@
+package proxy.pattern.demo;
+
+public class ProxyPatternEx {
+    public static void main(String[] args) {
+        System.out.println("Proxy Pattern Demo\n");
+        Proxy px = new Proxy();
+        px.doSomeWork();
+    }
+}
